9f00fba8a702ac2178abc87b54a409cf8d8b5d80
James Grantham <james.r.grantham@gmail.com>
2020-02-19 11:26:44 +0000

    Big O

--- W2D2-recursion.py
+++ W2D2-recursion.py
@@ -1,12 +1,10 @@
-sample = [1, 4, 3, 3, 4, 6, 7]
+sample = []
 
 def recurse_search(arr, target):
   if len(arr) == 0:
-    print('false')
     return False
   if arr[0] == target:
-    print('true')
     return True
   return recurse_search(arr[1:], target)
 
-recurse_search(sample, 8)
+print(recurse_search(sample, 3))
